e261783c389c990b3f566caba58fdbc1d035a76d
goonle <junhuh741@gmail.com>
2025-03-10 15:56:55 +1300

    maybe i created user for register and login

--- backend/urls.py
+++ backend/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('notes/', include('Note.urls')),
+    path('user/', include('User.urls')),
+
 ]
