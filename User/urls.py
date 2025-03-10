1af0f2396bfc81ed631509cebe3bc72ca52cc831
goonle <junhuh741@gmail.com>
2025-03-11 11:49:31 +1300

    To follow the assignment order, comment out some codes for login and logout

--- User/urls.py
+++ User/urls.py
@@ -4,8 +4,8 @@ from .views import RegisterView, LoginView, LogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    # path('login/', LoginView.as_view(), name='login'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
     ]
 
 
